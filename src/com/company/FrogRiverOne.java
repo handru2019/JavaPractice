0026a08db089410aed324a1a88e2e93b5c927b37
Gayk <monika.m.gaik@gmail.com>
2019-10-27 20:42:07 +0100

    Next tryie

--- src/com/company/FrogRiverOne.java
+++ src/com/company/FrogRiverOne.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
  * Created by monik on 27.10.2019.
  */
 public class FrogRiverOne {
+
     public static int solution(int X, int[] A) {
         HashSet<Integer> river = new HashSet<>(  );
         for (int i=0;i<A.length;i++){
@@ -19,7 +20,6 @@ public class FrogRiverOne {
         return -1;
     }
     public static void main(String[] args) {
-
         System.out.println(solution( 5,new int[]{1, 3, 1, 4, 2, 3, 5, 4} ));
     }
 }
